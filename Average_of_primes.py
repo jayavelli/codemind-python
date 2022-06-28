748616bd3810af90fcc56393d54229f3591cd3d0
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-06-28 17:38:51 +0530

    def prime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False         return True n=int(input()) a=list(map(int,input().split())) s=0 c=0 for i in range(n):     if prime(a[i]):         s+=a[i]         c+=1 a=s/c print("%.2f"%a)

---
+++ Average_of_primes.py
@@ -0,0 +1,18 @@
+def prime(n):
+    if n==1:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+        return True
+n=int(input())
+a=list(map(int,input().split()))
+s=0
+c=0
+for i in range(n):
+    if prime(a[i]):
+        s+=a[i]
+        c+=1
+a=s/c
+print("%.2f"%a)
