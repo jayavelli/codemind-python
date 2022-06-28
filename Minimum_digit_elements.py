b8383b60cfbffd6685707ef0e567f763e69bfe75
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-06-28 18:25:57 +0530

    n=int(input()) a=list(map(int,input().split())) s=0 min=999 c=0 for i in range(n):     if len(str(a[i]))<min:         min=len(str(a[i])) for i in range(n):     if len(str(a[i]))==min:         c+=1 print(c)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,12 @@
+n=int(input())
+a=list(map(int,input().split()))
+s=0
+min=999
+c=0
+for i in range(n):
+    if len(str(a[i]))<min:
+        min=len(str(a[i]))
+for i in range(n):
+    if len(str(a[i]))==min:
+        c+=1
+print(c)
