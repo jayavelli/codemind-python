31dd80942d86821ec4e38b3a696d495d46282b86
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-06-28 21:01:42 +0530

    def div(i):     n=i     c=0     while(i):         r=i%10         if r==0:             break         elif n%r==0:             c+=1         i//=10     return c a=int(input()) b=int(input()) for i in range(a,b+1):     if i<10:         print(i,end=' ')     else:         l=len(str(i))         p=div(i)         if l==p:             print(i,end=' ')

--- Self_Dividing_Numbers.py
+++ Self_Dividing_Numbers.py
@@ -1,19 +1,21 @@
-n=int(input())
-m=int(input())
-c=0
-f=0
-for i in range(n,m+1):
-    temp=i
+def div(i):
+    n=i
     c=0
-    f=0
-    while temp!=0:
-        r=temp%10
-        c+=1
-        if(r!=0):
-            if(i%r==0):
-                f+=1
-        temp//=10
-    if(c==f):
-        print(i,end=" ")
-        
-        
+    while(i):
+        r=i%10
+        if r==0:
+            break
+        elif n%r==0:
+            c+=1
+        i//=10
+    return c
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+    if i<10:
+        print(i,end=' ')
+    else:
+        l=len(str(i))
+        p=div(i)
+        if l==p:
+            print(i,end=' ')
