331a8dbccebba23f0732dd541726a7e0463b490f
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-06-30 21:19:05 +0530

    def rev(n):     t=n     s=0     while n!=0:         r=n%10         s=s*10+r         n//=10     return s n=int(input()) a=list(map(int,input().split())) for i in a:     print(rev(i),end=' ')

--- Reverse_game.py
+++ Reverse_game.py
@@ -1,18 +1,12 @@
 def rev(n):
-    p=n
-    r=0
-    while n:
-        k=n%10
-        r=r*10+k
+    t=n
+    s=0
+    while n!=0:
+        r=n%10
+        s=s*10+r
         n//=10
-    return r
-
+    return s
 n=int(input())
 a=list(map(int,input().split()))
-c=0
-k=[]
-for i in range(n):
-    k.append(rev(a[i]))
-for i in range(n):
-    print(k[i],end=' ')
-    
+for i in a:
+    print(rev(i),end=' ')
