2794340c45d7ef3e6bc083e493d1ef298c728d0b
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-06-30 20:40:39 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) k=[] for i in a:     if i not in k and i in b:         k.append(i) print(*k)

--- Common_elements.py
+++ Common_elements.py
@@ -1,11 +1,8 @@
-
 n,m=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
-
 k=[]
 for i in a:
-    if i in b and i not in k  :
+    if i not in k and i in b:
         k.append(i)
 print(*k)
-    
