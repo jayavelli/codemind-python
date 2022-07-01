294b3fbeb2b5983a2a6c17cc4393e6a554b0102c
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-07-01 10:53:39 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 k=[] s=0 for i in range(n):     if a[i]!=-1:         c=1         for j in range (n):             if a[i]==a[j] and i!=j:                 c+=1                 a[j]=-1
            if a[i]==c:
                s+=1
    print(s)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,18 +1,16 @@
 n=int(input())
 a=list(map(int,input().split()))
+c=0
 k=[]
 s=0
-c=0
-p=0
 for i in range(n):
     if a[i]!=-1:
         c=1
-        for j in range(n):
+        for j in range (n):
             if a[i]==a[j] and i!=j:
                 c+=1
                 a[j]=-1
+                
         if a[i]==c:
-            k.append(a[i])
-for i in range(len(k)):
-    p+=1
-print(p)
+            s+=1
+print(s)
