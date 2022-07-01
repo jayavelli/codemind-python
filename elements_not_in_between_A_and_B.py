cb10020fa42d3b1a18cdfd02291bd09f94f7bef8
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-07-01 11:08:04 +0530

    n=int(input()) a=list(map(int,input().split())) l,m=map(int,input().split()) c=0 k=[] s=0 max=0 for i in range(n):     if a[i]<l or  a[i]>m:         c+=1         k.append(a[i]) if c==0:     print(-1) else:     print(*k)

---
+++ elements_not_in_between_A_and_B.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=list(map(int,input().split()))
+l,m=map(int,input().split())
+c=0
+k=[]
+s=0
+max=0
+for i in range(n):
+    if a[i]<l or  a[i]>m:
+        c+=1
+        k.append(a[i])
+if c==0:
+    print(-1)
+else:
+    print(*k)
