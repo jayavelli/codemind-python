e67c41c6850958a204731c3ddeb3bd939617874e
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-07-03 18:15:55 +0530

    a,b=map(int,input().split()) l=[] for i in range(a):     k=list(map(int,input().split()))     if len(k)==b:         l.append(k)
    e=0
    o=0
    for i in range(a):
        for j in range(b):
            e+=l[i][j]
    print(e)

---
+++ Sum_of_all_elements_in_a_matrix.py
@@ -0,0 +1,13 @@
+a,b=map(int,input().split())
+l=[]
+for i in range(a):
+    k=list(map(int,input().split()))
+    if len(k)==b:
+        l.append(k)
+        
+e=0
+o=0
+for i in range(a):
+    for j in range(b):
+        e+=l[i][j]
+print(e)
