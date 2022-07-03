c3f67896b9034856898cf78be62d4a95f101adf9
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-07-03 18:20:21 +0530

    a,b=map(int,input().split()) l=[] for i in range(a):     k=list(map(int,input().split()))     if len(k)==b:         l.append(k)
    e=0
    o=0
    for j in range(b):
        for i in range(a):
            e+=l[i][j]
        if o<e:
            o=e
        e=0
    print(o)

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.py
@@ -0,0 +1,17 @@
+a,b=map(int,input().split())
+l=[]
+for i in range(a):
+    k=list(map(int,input().split()))
+    if len(k)==b:
+        l.append(k)
+        
+e=0
+o=0
+for j in range(b):
+    for i in range(a):
+        e+=l[i][j]
+    if o<e:
+        o=e
+    e=0
+print(o)
+    
