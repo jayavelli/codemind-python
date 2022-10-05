1ff2e05eb0198e744c7dbefad3d7ec6b5d8028d5
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-10-05 21:16:17 +0530

    n=int(input()) for i in reversed(range(1,n+1)):     for j in reversed(range(1,n+1)):         if i==j or i+j==n+1:             print(i,end=" ")         else:             print(" ",end="")     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,6 +1,8 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(i,n+1):
-        print(j,end=" ")
+for i in reversed(range(1,n+1)):
+    for j in reversed(range(1,n+1)):
+        if i==j or i+j==n+1:
+            print(i,end=" ")
+        else:
+            print(" ",end="")
     print()
-    
