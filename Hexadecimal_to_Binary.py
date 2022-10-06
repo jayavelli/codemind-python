c216b1d5af624279092b289068ff6f4ef62161f2
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-10-06 12:05:03 +0530

    def hex(n):     for i in n:         if i=='0':             print("0000",end="")         elif i=='1':             print("0001",end="")         elif i=='2':             print("0010",end="")         elif i=='3':             print("0011",end="")         elif i=='4':             print("0100",end="")         elif i=='5':             print("0101",end="")         elif i=='6':             print('0110',end="")         elif i=='7':             print("0111",end='')         elif i=='8':             print("1000",end='')         elif i=='9':             print('1001',end="")         elif i=='A':             print('1010',end="")         elif i=='B':             print("1011",end="")         elif i=="C":             print("1100",end="")         elif i=="D":             print("1101",end="")         elif i=="E":             print("1110",end="")         elif i=="F":             print("1111",end="")     print() n=int(input()) for i in range(n):     a=input()     hex(a)

---
+++ Hexadecimal_to_Binary.py
@@ -0,0 +1,41 @@
+def hex(n):
+    for i in n:
+        if i=='0':
+            print("0000",end="")
+        elif i=='1':
+            print("0001",end="")
+        elif i=='2':
+            print("0010",end="")
+        elif i=='3':
+            print("0011",end="")
+        elif i=='4':
+            print("0100",end="")
+        elif i=='5':
+            print("0101",end="")
+        elif i=='6':
+            print('0110',end="")
+        elif i=='7':
+            print("0111",end='')
+        elif i=='8':
+            print("1000",end='')
+        elif i=='9':
+            print('1001',end="")
+        elif i=='A':
+            print('1010',end="")
+        elif i=='B':
+            print("1011",end="")
+        elif i=="C":
+            print("1100",end="")
+        elif i=="D":
+            print("1101",end="")
+        elif i=="E":
+            print("1110",end="")
+        elif i=="F":
+            print("1111",end="")
+    print()
+n=int(input())
+for i in range(n):
+    a=input()
+    hex(a)
+        
+        
