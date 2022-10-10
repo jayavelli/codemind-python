966673649ac38876b9289662f3b252dc490b277b
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-10-10 22:05:21 +0530

    n=int(input()) k = n-1 for i in range(0, n):     for j in range(0, k):         print(end=" ")     k-=1     for j in range(1, i+2):
            # printing stars
            print(j, end="")
        for j in range(i,0,-1):
            print(j, end="")
        print()

--- Pattern.py
+++ Pattern.py
@@ -1,6 +1,13 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(i,end=" ")
+k = n-1
+for i in range(0, n):
+    for j in range(0, k):
+        print(end=" ")
+    k-=1
+    for j in range(1, i+2):
+     
+        # printing stars
+        print(j, end="")
+    for j in range(i,0,-1):
+        print(j, end="")
     print()
-    
