169bc14d215e0144dd9964aca66d815c25e4e6dc
jayavelli <96563831+jayavelli@users.noreply.github.com>
2022-10-10 21:43:41 +0530

    def gcd(a,b):     if a%b==0:         return b     return gcd(b,a%b) t=int(input()) for i in range(t):     n,a,b,k=map(int,input().split())     tot=(n//a)+(n//b)     l=(a*b)//gcd(a,b)     tot-=(2*(n//l))     if tot>=k:         print("Win")     else:         print("Lose")

---
+++ Appy_and_Contest.py
@@ -0,0 +1,14 @@
+def gcd(a,b):
+    if a%b==0:
+        return b
+    return gcd(b,a%b)
+t=int(input())
+for i in range(t):
+    n,a,b,k=map(int,input().split())
+    tot=(n//a)+(n//b)
+    l=(a*b)//gcd(a,b)
+    tot-=(2*(n//l))
+    if tot>=k:
+        print("Win")
+    else:
+        print("Lose")
